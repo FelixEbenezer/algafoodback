402aee4d8aedf790ef8e451b935d23749dce9b60
Nga <felixwhite2008@hotmail.com>
2020-06-28 15:18:30 +0200

    Corrigindo_problemas_anteriores_chave_publica

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,13 +1,9 @@
 package com.algaworks.algafood.core.security;
-import javax.crypto.spec.SecretKeySpec;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
@@ -17,18 +13,21 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.anyRequest().authenticated()
+			//	.antMatchers(HttpMethod.POST, "/v1/cozinhas").permitAll()
+			.antMatchers("/v1/usuarios").permitAll()
+			.anyRequest().authenticated()
+			
 			.and()
-			.cors().and()
-			.oauth2ResourceServer()
-			//.opaqueToken();
-			.jwt();
+				.cors().and()
+				.oauth2ResourceServer()
+				//.opaqueToken();
+				.jwt();
 	}
 	
-	@Bean
+/*	@Bean
 	public JwtDecoder jwtDecoder() {
 	 var secretKey = new SecretKeySpec("algaworksalgaworks123456789algaworksalgaworks123456789algaworksalgaworks123456789".getBytes(), "HmacSHA256");
 	 return NimbusJwtDecoder.withSecretKey(secretKey).build();
 	}
-	
+	*/
 }
